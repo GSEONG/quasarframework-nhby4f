577e086c3fc9f5bccfe9328a9f65358f7d697681
GSEONG <cgs4323@naver.com>
2022-12-13 18:03:08 +0900

    3 files modified

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -3,27 +3,43 @@
     class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
   >
     <div>
-      <div style="font-size: 30vh">Login</div>
+      <div class="text-bold text-indigo-8" style="font-size: 4em">Login</div>
 
-      <div class="text-h2" style="opacity: 0.4">ID:</div>
-      <div class="text-h2" style="opacity: 0.4">PW:</div>
+      <q-form @submit.prevent="onSubmit">
+        <q-input v-model="id" type="text" label="ID" required />
+        <q-input v-model="password" type="password" label="Password" required />
+        <div class="q-my-sm">(ID: test / password: 1234)</div>
 
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/index"
-        label="Go Home"
-        no-caps
-        @click="login"
-      />
+        <q-btn
+          type="submit"
+          class="q-mt-xl"
+          color="white"
+          text-color="blue"
+          unelevated
+          label="Login"
+          no-caps
+        >
+          <q-icon name="login" />
+        </q-btn>
+      </q-form>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const id = ref('');
+const password = ref('');
 const login = () => {
-  localStorage.setItem('test', 1234);
+  if (id.value === 'test' && password.value === '1234') {
+    localStorage.setItem('test', 1234);
+    router.push('index');
+  }
+};
+const onSubmit = () => {
+  login();
 };
 </script>
--- src/pages/StreamPage.vue
+++ src/pages/StreamPage.vue
@@ -8,17 +8,74 @@
         label="OnAir"
       />
       <q-btn text-color="black" label="새로고침" />
-      <q-btn text-color="black" label="설정" />
+      <q-btn text-color="black" label="설정" @click="dialog = true" />
     </nav>
+
     <TableView />
+    <q-dialog v-model="dialog" persistent full-width full-height>
+      <q-card class="column no-wrap">
+        <q-card-section v-for="num in 2" :key="num" class="column items-center">
+          <div class="text-h6">{{ num }}번</div>
+          <q-input label="url 1" class="input-style" />
+          <q-input label="url 2" class="input-style" />
+          <q-input label="url 3" class="input-style" />
+          <q-select
+            v-model="resolution"
+            :options="resolOptions"
+            label="화질 선택"
+            class="input-style"
+          />
+          <q-input
+            label="비디오 비트레이트"
+            type="number"
+            class="input-style"
+            v-model="videoBitrate"
+            max="10"
+            min="1"
+            :rules="[
+              (val) => (val > 0 && val <= 10) || '1에서 10사이의 정수를 입력',
+            ]"
+          />
+          <q-select
+            v-model="audioBitrate"
+            :options="audioOptions"
+            label="오디오 비트레이트"
+            class="input-style"
+          />
+        </q-card-section>
+
+        <!-- Notice v-close-popup -->
+        <q-card-actions align="right" class="content-stretch">
+          <q-btn label="저장하기" color="primary" v-close-popup />
+          <q-btn flat label="취소" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import TableView from 'components/TableView.vue';
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar();
 
 const onairColor = ref('yellow');
+const dialog = ref(false);
+const cancelEnabled = ref(false);
+
+const resolution = ref('');
+const resolOptions = ref([
+  { label: 'FHD(1920 X 1080)', value: 'FHD' },
+  { label: 'HD(1280 X 720)', value: 'HD' },
+]);
+const videoBitrate = ref(5);
+const audioBitrate = ref('');
+const audioOptions = ref([
+  { label: '128kbps', value: 128 },
+  { label: '192kbps', value: 192 },
+]);
 
 const changeCondition = () => {
   // 작동 중일 때만!
@@ -28,4 +85,31 @@ const changeCondition = () => {
     onairColor.value = 'yellow';
   }
 };
+const prompt = () => {
+  $q.dialog({
+    title: 'Prompt',
+    message: 'What is your name?',
+    prompt: {
+      model: '',
+      type: 'text', // optional
+    },
+    cancel: true,
+    persistent: true,
+  })
+    .onOk((data) => {
+      console.log('>>>> OK, received', data);
+    })
+    .onCancel(() => {
+      // console.log('>>>> Cancel')
+    })
+    .onDismiss(() => {
+      // console.log('I am triggered on both OK and Cancel')
+    });
+};
 </script>
+
+<style lang="scss">
+.input-style {
+  width: max(200px, 50%);
+}
+</style>
